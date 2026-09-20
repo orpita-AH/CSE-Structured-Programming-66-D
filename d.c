f1a5debe5a588f385f694ce14fc8506c30cd0dcc
orpita-AH <orpitaattitude@gmail.com>
2026-09-20 16:41:08 +0600

    Add files via upload

---
+++ d.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+int a,b,c,d;
+char A,B,C,D;
+scanf("%c %d %c %d %c %d %c %d",&A,&a,&B,&b,&C,&c,&D,&d);
+printf("%c=%d\n%c=%d\n%c=%d\n%c=%d",A,a,B,b,C,c,D,d);
+
+return 0;
+
+
+
+
+}
