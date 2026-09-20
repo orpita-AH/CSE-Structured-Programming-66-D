82cf009118290d73737c33d138a8b902071175ba
orpita-AH <orpitaattitude@gmail.com>
2026-09-20 16:42:26 +0600

    Add files via upload

---
+++ Class 1/YourWork/c.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main (){
+    int a,b;
+    char o;
+    scanf("%d %d %c",&a,&b,&o);
+    printf("%d %c",a+b,o);
+    return 0;
+
+
+
+
+
+}
---
+++ Class 1/YourWork/e.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+int a, b;
+scanf("%d %d",&a, &b);
+int sum=a+b;
+if(sum%2==0){
+printf("sum is even\n");
+}else{
+    printf("sum is odd\n");
+}
+return 0;
+
+
+
+
+}
